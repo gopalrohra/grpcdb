20332a3052cea06810dac76d163402785a2f7801
Gopal Rohra <gopal.rohra@gmail.com>
2021-04-24 20:33:23 +0530

    Modified run script to exit on error and also clean up the bin directory before compile Added prod-push script to compile binaries for production mode. Added couple of patterns in .gitignore Added env.go to implement multiple environment support.

---
+++ env.go
@@ -0,0 +1,47 @@
+package env
+
+import (
+	"fmt"
+	"io/ioutil"
+	"os"
+	"path/filepath"
+	"strings"
+)
+
+// Config to hold environment based configuration
+var Config = map[string]string{}
+
+// LoadEnvironment loads .env for production and .env.<env_name> for specific environment
+func LoadEnvironment() {
+	//set mode to production, development, staging etc
+	mode := os.Getenv("GO_ENV")
+	if mode == "" {
+		mode = "production"
+	}
+	dir, err := filepath.Abs(filepath.Dir(os.Args[0]))
+	if err != nil {
+		fmt.Println(err.Error())
+	}
+	fmt.Println(dir)
+	env_file := dir + "/.env"
+	if mode != "production" {
+		env_file = env_file + "." + mode
+	}
+	data, err := ioutil.ReadFile(env_file)
+	if err != nil {
+		fmt.Println(err.Error())
+	}
+	lines := strings.Split(fmt.Sprintf("%s", data), "\n")
+	fmt.Println(len(lines))
+	for _, line := range lines {
+		fmt.Println("Inside loop")
+		fmt.Println(line)
+		kvPair := strings.Split(line, "=")
+		if len(kvPair) == 2 {
+			if strings.Trim(kvPair[0], " ") != "" && strings.Trim(kvPair[1], " ") != "" {
+				Config[kvPair[0]] = kvPair[1]
+			}
+		}
+	}
+	fmt.Println(config)
+}
